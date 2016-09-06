69ab71f76796578311482300451fa44c20222646
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-09-06 22:31:30 +0200

    Creado primer listado con photos en la home

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from photos.models import Photo
 
 
-def hello(request):
-    nombre = request.GET.get('nombre')
-    apellido = request.GET.get('apellido')
+def home(request):
+    """
+    Renderiza el home con un listado de fotos
+    :param request: objeto HttpRequest con los datos de la petición
+    :return: objeto HttpResponse con los datos de la respuesta
+    """
+    photos = Photo.objects.all  # Acceso al ModelManager del modelo. Recupera todas las fotos de la base de datos
+    context = {'photos_list': photos}
 
-    return HttpResponse("Hello {0} {1}".format(nombre, apellido))
+    return render(request, 'photos/home.html', context)
