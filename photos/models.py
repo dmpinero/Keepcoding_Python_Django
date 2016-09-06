4e6746ee87f59f77826d0b015c0d5c4faff0a1b9
Daniel Martínez Piñero <dmpinero@gmail.com>
2016-09-06 21:44:51 +0200

    Administracion de Django

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+
+    LICENSES = (
+        ('RIG', 'Copyright'),
+        ('LEF', 'Copyleft'),
+        ('CC', 'Creative commons')
+    )
+
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField()                   # Se diferencia del Charfield en que un TextField no tiene límite
+    license = models.CharField(max_length=3, choices=LICENSES)
+    created_at = models.DateTimeField(auto_now_add=True) # Sólo se actualiza cuando se crea
+    modified_at = models.DateTimeField(auto_now=True)    # Se actualiza cuando se crea y cada vez que se actualiza
+
+    def __str__(self):
+        return self.name
