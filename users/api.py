9f5f693415309b4d68539c658d6ad1225251fcf1
Daniel Martinez <dmpinero@gmail.com>
2016-09-14 00:31:36 +0200

    Creamos el endpoint de creacion de un usuario (a medias)

--- users/api.py
+++ users/api.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from users.serializers import UserSerializer
 
 
@@ -13,3 +13,11 @@ class UserListAPI(APIView):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)  # Convierte lista de usuarios en lista de diccionarios
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
@@ -5,11 +6,19 @@ class UserSerializer(serializers.Serializer):
 
     first_name = serializers.CharField()
     last_name = serializers.CharField()
-
+    username = serializers.CharField()
+    password = serializers.CharField()
+    email = serializers.EmailField()
 
     def create(self, validated_data):
-        pass
-
+        instance = User()
+        instance.first_name =validated_data.get('first_name')
+        instance.last_name = validated_data.get('last_name')
+        instance.username = validated_data.get('username')
+        instance.password = validated_data.get('password')
+        instance.email = validated_data.get('email')
+        instance.save()
+        return instance
 
     def update(self, instance, validated_data):
         pass
