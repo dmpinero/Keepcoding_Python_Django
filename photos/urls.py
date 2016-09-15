bbba06c28e06446ea6dc53530bcc8ddaa835b898
Daniel Martinez <dmpinero@gmail.com>
2016-09-15 21:41:11 +0200

    Implementamos endpoint de listado y creacion de fotos

--- photos/urls.py
+++ photos/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+
+from photos.api import PhotoListAPI
 from photos.views import HomeView, PhotoDetailView, PhotoCreationView, PhotoListView
 
 
@@ -6,5 +8,8 @@ urlpatterns = [
     url(r'^create$', PhotoCreationView.as_view(), name='photos_create'),
     url(r'^photos/$', PhotoListView.as_view(), name='photos_my_photos'),
     url(r'^photos/(?P<pk>\d+)$', PhotoDetailView.as_view(), name='photos_detail'), # \d Patrón para uno o más números
-    url(r'^$', HomeView.as_view(), name='photos_home')
+    url(r'^$', HomeView.as_view(), name='photos_home'),
+
+    # API
+    url(r'^api/1.0/photos/$', PhotoListAPI.as_view(), name='api_photos_list')
 ]
--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
 
     # API URLS
     url(r'^api/1.0/users/$', UserListAPI.as_view(), name='api_user_list'),
-    url(r'^api/1.0/users/(?P<pk>[0-9]+)$', UserDetailAPI.as_view(), name='api_user_detail')
+    url(r'^api/1.0/users/(?P<pk>[0-9]+)$', UserDetailAPI.as_view(), name='api_user_detail'),
 ]
