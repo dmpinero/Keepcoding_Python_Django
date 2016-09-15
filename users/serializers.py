7f03110a28026e72a4dd89723b918a5990b6cd30
Daniel Martinez <dmpinero@gmail.com>
2016-09-15 21:22:10 +0200

    REST con Serializadores

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,20 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
+class UserListSerializer(serializers.Serializer):
+
+    # Campos a devolver en la respuesta
+    id = serializers.ReadOnlyField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+
 class UserSerializer(serializers.Serializer):
 
+    # Campos a devolver en la respuesta
+    id = serializers.ReadOnlyField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
     username = serializers.CharField()
@@ -12,13 +23,14 @@ class UserSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         instance = User()
+        return self.update(instance, validated_data)
+
+    def update(self, instance, validated_data):
         instance.first_name =validated_data.get('first_name')
         instance.last_name = validated_data.get('last_name')
         instance.username = validated_data.get('username')
-        instance.password = validated_data.get('password')
+        instance.password = make_password(validated_data.get('password'))
         instance.email = validated_data.get('email')
         instance.save()
         return instance
 
-    def update(self, instance, validated_data):
-        pass
